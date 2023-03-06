7bab60a9f8923547adaf86ff74ec4b7de7ad31e4
anandraval4 <anandraval.mu@gmai.com>
2023-03-07 00:22:06 +0530

    4.Write a java program to swap to numbers without using third variable
    4.Write a java program to swap to numbers without using third variable

---
+++ 04_swapnum.java
@@ -0,0 +1,12 @@
+class swapnum
+{
+    public static void main(String args[])
+    {
+        int a=30,b=10;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("A is "+a);
+        System.out.println("B  is "+b);
+    }   
+}
