6bb6442987d550d27899dcbe6fe72067141e44ac
anandraval4 <anandraval.mu@gmai.com>
2023-03-07 00:20:23 +0530

    3.Address as follows 1.single print statement  2.multi-ple print statement
    3.Address as follows
    1.single print statement
    2.multi-ple print statement

---
+++ 03_address.java
@@ -0,0 +1,11 @@
+class address 
+{
+    public static void main(String args[])
+    {
+        //Single line
+        System.out.println("Marwadi University");
+        //Double line
+        System.out.println("Marwadi University");
+        System.out.println("Morbi highway rajkot");    
+    }
+}
